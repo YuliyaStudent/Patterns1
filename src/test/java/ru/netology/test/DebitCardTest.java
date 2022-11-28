cca17a368767c66fa1c31755618bb8cd32b1283a
YuliyaSi <pirozhok_s_kapustpo@mail.ru>
2022-11-28 20:39:59 +0300

    add lombok

--- src/test/java/ru/netology/test/DebitCardTest.java
+++ src/test/java/ru/netology/test/DebitCardTest.java
@@ -1,4 +1,4 @@
-package ru.netology;
+package ru.netology.test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,15 +20,19 @@ public class DebitCardTest {
 
     @Test
     @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() {
+    private void shouldSuccessfulPlanAndReplanMeeting() {
         $("[data-test-id=city] input").setValue(DataGenerator.generateCity());
         $("[data-test-id=date] input").sendKeys(DELETE, DataGenerator.generateDate(3));
         $("[data-test-id=name] input").sendKeys(DataGenerator.generateName("ru"));
-        $("[data-test-id=phone] input]").sendKeys(DataGenerator.generatePhone("ru"));
+        $("[data-test-id=phone] input").sendKeys(DataGenerator.generatePhone("ru"));
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $("[data-test-id=notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).
                 should(exactText("Встреча успешно забронирована на " + DataGenerator.generateDate(3)));
-
+        $("[data-test-id=date] input").sendKeys(DELETE, DataGenerator.generateDate(7));
+        $("button.button").click();
+        $("[data-test-id =replan-notification] .notification__content").shouldBe(visible,Duration.ofSeconds(15)).should(exactText("Необходимо подтверждение"));
+        $("[data-test-id=replan-notification] button").click();
+        $("[data-test-id=success-notification] .notification__content").shouldBe(visible,Duration.ofSeconds(15)).shouldHave(exactText("Встреча успешно забронирована на"  + DataGenerator.generateDate(7)));
     }
 }
